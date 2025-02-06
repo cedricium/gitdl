1ceb1bda675966969033cdb3fbedf7839eaec14d
Cedric Amaya <amaya.cedric@gmail.com>
2025-02-06 14:38:31 -0800

    fix: better error handling around existing files

--- fs.go
+++ fs.go
@@ -15,7 +15,10 @@ func (fm *FileManager) ValidateDestDir() error {
 	dstInfo, err := os.Stat(fm.DestDir)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return os.MkdirAll(fm.DestDir, 0755)
+			// if destination does not exist, skip validation since the given
+			// destination will be created in `fm.MoveFiles`. if created here,
+			// `os.Rename` operation will fail due to an existing newpath
+			return nil
 		}
 		return err
 	}
@@ -40,7 +43,11 @@ func (fm *FileManager) MoveFiles(sources []string, tempDir string) error {
 		dst := filepath.Join(fm.DestDir, ford)
 		if err := os.Rename(src, dst); err != nil {
 			if errors.Is(err, os.ErrExist) {
-				// overwrite (delete) existing directory here
+				// if options.Force:
+				// 	— delete existing directory dst
+				// 	- return nil to continue with overwrite
+				// else:
+				return fmt.Errorf("directory with content at `%s' would be overwritten, skipping…", dst)
 			}
 			return err
 		}
